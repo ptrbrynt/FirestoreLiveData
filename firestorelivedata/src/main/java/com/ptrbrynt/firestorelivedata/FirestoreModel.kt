90bde340783f08c06e985626c6d08d3af0976f78
Peter Bryant <peter@rotacloud.com>
2018-06-28 16:38:58 +0100

    Add required annotations to FirestoreModel

--- firestorelivedata/src/main/java/com/ptrbrynt/firestorelivedata/FirestoreModel.kt
+++ firestorelivedata/src/main/java/com/ptrbrynt/firestorelivedata/FirestoreModel.kt
@@ -1,5 +1,10 @@
 package com.ptrbrynt.firestorelivedata
 
+import com.google.firebase.firestore.Exclude
+import com.google.firebase.firestore.IgnoreExtraProperties
+
+@IgnoreExtraProperties
 abstract class FirestoreModel{
+    @Exclude
     var id: String? = null
 }
