0571d1c2ba6f4bcddfc2cb49b29efa28dba03fea
Peter Bryant <peter@rotacloud.com>
2018-06-29 13:53:25 +0100

    Document CollectionLiveData

--- firestorelivedata/src/main/java/com/ptrbrynt/firestorelivedata/CollectionLiveData.kt
+++ firestorelivedata/src/main/java/com/ptrbrynt/firestorelivedata/CollectionLiveData.kt
@@ -4,8 +4,23 @@ import android.arch.lifecycle.LiveData
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.DocumentReference
 
+/**
+ * An observable [LiveData] representing the current state of the data at a [CollectionReference].
+ *
+ * @param T The [FirestoreModel] to convert the resulting Firestore documents into
+ * @param modelClass The class representing [T]
+ * @param collectionReference The [CollectionReference] to provide the [CollectionLiveData] for.
+ *
+ * @see [CollectionReference.asLiveData]
+ * @see [FirestoreResource]
+ * @see [FirestoreModel]
+ */
 class CollectionLiveData<T : FirestoreModel>(private val modelClass: Class<T>, private val collectionReference: CollectionReference) : LiveData<FirestoreResource<List<T>>>() {
 
+    /**
+     * When the instance becomes active, initially post a [Status.LOADING] value.
+     * Then, call [CollectionReference.addSnapshotListener] to listen for data changes, and post new values to the [CollectionLiveData] instance appropriately.
+     */
     override fun onActive() {
         super.onActive()
         postValue(FirestoreResource.loading())
@@ -20,6 +35,14 @@ class CollectionLiveData<T : FirestoreModel>(private val modelClass: Class<T>, p
         }
     }
 
+    /**
+     * Adds a new item to the collection referred to by [collectionReference].
+     *
+     * This is returned as a [TaskLiveData], so the state of the operation can be observed if desired. However, it is not required to be observed in order to execute the operation.
+     *
+     * @param item The item to add to the collection
+     * @return A [TaskLiveData] representing the state of the operation, as a [TaskResult].
+     */
     fun add(item: T): TaskLiveData<DocumentReference> = collectionReference.add(item).asLiveData()
 
 }
