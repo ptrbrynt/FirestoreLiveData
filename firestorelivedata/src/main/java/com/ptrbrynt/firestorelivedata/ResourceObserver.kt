afc0e86def3629750aff318ac5fcbd9d2b77aa21
Peter Bryant <peter@rotacloud.com>
2018-06-29 11:43:49 +0100

    Include errormessage in ResourceObserver error callback

--- firestorelivedata/src/main/java/com/ptrbrynt/firestorelivedata/ResourceObserver.kt
+++ firestorelivedata/src/main/java/com/ptrbrynt/firestorelivedata/ResourceObserver.kt
@@ -6,14 +6,14 @@ interface ResourceObserver<T> : Observer<FirestoreResource<T>> {
 
     fun onSuccess(data: T?)
 
-    fun onError(throwable: Throwable?)
+    fun onError(throwable: Throwable?, errorMessage: String?)
 
     fun onLoading()
 
     override fun onChanged(t: FirestoreResource<T>?) {
         when (t?.status) {
             Status.SUCCESS -> onSuccess(t.data)
-            Status.ERROR -> onError(t.throwable)
+            Status.ERROR -> onError(t.throwable, t.errorMessage)
             Status.LOADING -> onLoading()
         }
     }
